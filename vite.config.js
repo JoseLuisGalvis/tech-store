17d75a0c604460170c91351526c6c3e5a034f0fe
proyectos-jogal <jogal1965jjgh@gmail.com>
2025-10-17 21:33:51 -0300

    Ajuste vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/techstore/", // Reemplaza con el nombre exacto de tu repo
+  base: "/tech-store/", // Reemplaza con el nombre exacto de tu repo
 });
